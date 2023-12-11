4db93719b4e8eab48c802214defcd52b70cb973a
thezzisu <thezzisu@gmail.com>
2023-12-12 00:38:25 +0800

    chore: update ci

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 BUILD_DIR=$(dirname "$0")/build/releases
+rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
-cd $BUILD_DIR
 
 sum="sha256sum"
 VERSION=$(git describe --tags --always --dirty)
 
-# AMD64 
+# AMD64
 OSES=(linux darwin windows freebsd)
 for os in ${OSES[@]}; do
 	suffix=""
@@ -15,11 +15,11 @@ for os in ${OSES[@]}; do
 	then
 		suffix=".exe"
 	fi
-	env GOOS=$os GOARCH=amd64 go build -o azukiiro_${os}_amd64${suffix} github.com/zhzxdev/azukiiro
-	tar -zcf azukiiro-${os}-amd64-$VERSION.tar.gz azukiiro_${os}_amd64${suffix}
-  rm azukiiro_${os}_amd64${suffix}
-	HASH=$($sum azukiiro-${os}-amd64-$VERSION.tar.gz)
-  echo "$HASH azukiiro-${os}-amd64-$VERSION.tar.gz" > azukiiro-$VERSION.sha256
+	env GOOS=$os GOARCH=amd64 go build -o $BUILD_DIR/azukiiro_${os}_amd64${suffix}
+	tar -zcf $BUILD_DIR/azukiiro-${os}-amd64-$VERSION.tar.gz $BUILD_DIR/azukiiro_${os}_amd64${suffix}
+  rm $BUILD_DIR/azukiiro_${os}_amd64${suffix}
+	HASH=$($sum $BUILD_DIR/azukiiro-${os}-amd64-$VERSION.tar.gz)
+  echo "$HASH" >> $BUILD_DIR/azukiiro-$VERSION.sha256
 done
 
 # 386
@@ -30,21 +30,21 @@ for os in ${OSES[@]}; do
 	then
 		suffix=".exe"
 	fi
-	env GOOS=$os GOARCH=386 go build -o azukiiro_${os}_386${suffix} github.com/zhzxdev/azukiiro
-	tar -zcf azukiiro-${os}-386-$VERSION.tar.gz azukiiro_${os}_386${suffix}
-  rm azukiiro_${os}_386${suffix}
-	HASH=$($sum azukiiro-${os}-386-$VERSION.tar.gz)
-  echo "$HASH azukiiro-${os}-386-$VERSION.tar.gz" > azukiiro-$VERSION.sha256
+	env GOOS=$os GOARCH=386 go build -o $BUILD_DIR/azukiiro_${os}_386${suffix}
+	tar -zcf $BUILD_DIR/azukiiro-${os}-386-$VERSION.tar.gz $BUILD_DIR/azukiiro_${os}_386${suffix}
+  rm $BUILD_DIR/azukiiro_${os}_386${suffix}
+	HASH=$($sum $BUILD_DIR/azukiiro-${os}-386-$VERSION.tar.gz)
+  echo "$HASH" >> $BUILD_DIR/azukiiro-$VERSION.sha256
 done
 
 # ARM
 ARMS=(5 6 7)
 for v in ${ARMS[@]}; do
-	env GOOS=linux GOARCH=arm GOARM=$v go build -o azukiiro_linux_arm$v  github.com/zhzxdev/azukiiro
-  tar -zcf azukiiro-linux-arm$v-$VERSION.tar.gz azukiiro_linux_arm$v
-  rm azukiiro_linux_arm$v
-  HASH=$($sum azukiiro-linux-arm$v-$VERSION.tar.gz)
-  echo "$HASH azukiiro-linux-arm$v-$VERSION.tar.gz" > azukiiro-$VERSION.sha256
+	env GOOS=linux GOARCH=arm GOARM=$v go build -o $BUILD_DIR/azukiiro_linux_arm$v 
+  tar -zcf $BUILD_DIR/azukiiro-linux-arm$v-$VERSION.tar.gz $BUILD_DIR/azukiiro_linux_arm$v
+  rm $BUILD_DIR/azukiiro_linux_arm$v
+  HASH=$($sum $BUILD_DIR/azukiiro-linux-arm$v-$VERSION.tar.gz)
+  echo "$HASH" >> $BUILD_DIR/azukiiro-$VERSION.sha256
 done
 
 # ARM64
@@ -55,9 +55,9 @@ for os in ${OSES[@]}; do
 	then
 		suffix=".exe"
 	fi
-	env GOOS=$os GOARCH=arm64 go build -o azukiiro_${os}_arm64${suffix} github.com/zhzxdev/azukiiro
-	tar -zcf azukiiro-${os}-arm64-$VERSION.tar.gz azukiiro_${os}_arm64${suffix}
-  rm azukiiro_${os}_arm64${suffix}
-  HASH=$($sum azukiiro-${os}-arm64-$VERSION.tar.gz)
-  echo "$HASH azukiiro-${os}-arm64-$VERSION.tar.gz" > azukiiro-$VERSION.sha256
+	env GOOS=$os GOARCH=arm64 go build -o $BUILD_DIR/azukiiro_${os}_arm64${suffix}
+	tar -zcf $BUILD_DIR/azukiiro-${os}-arm64-$VERSION.tar.gz $BUILD_DIR/azukiiro_${os}_arm64${suffix}
+  rm $BUILD_DIR/azukiiro_${os}_arm64${suffix}
+  HASH=$($sum $BUILD_DIR/azukiiro-${os}-arm64-$VERSION.tar.gz)
+  echo "$HASH" >> $BUILD_DIR/azukiiro-$VERSION.sha256
 done
